c42a87a57f91f585bea05f63dc9326bae63e8ed8
ohanSuyal <144371692+Adirohansuyal@users.noreply.github.com>
2025-02-21 16:26:46 +0530

    initial commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def load_data():
 df = load_data()
 
 # Title & Dataset Preview
-st.title("🌺 Iris Flower Classification by Aditya Suyal")
+st.title("🌺 Iris Flower Classification")
 st.write("A simple ML model trained on the Iris dataset demonstrating Github Actions for future updates.")
 
 st.write("### Sample Data")

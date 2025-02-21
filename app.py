0189e9091a008cb5eb6fae635670ddae6df7224d
ohanSuyal <144371692+Adirohansuyal@users.noreply.github.com>
2025-02-21 18:45:45 +0530

    Changes in App Title version 2.0

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def train_model(model_type="Random Forest", n_estimators=100, kernel="rbf"):
     return model, accuracy
 
 # App Layout
-st.title("🌺 Iris Flower Classification Model By Aditya Suyal")
+st.title("🌺 Iris Flower Classification Model")
 st.write("A Streamlit app demonstrating GitHub Actions with automated feature updates (CI/CD).")
 
 # Sidebar Navigation

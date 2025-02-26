5ffb8d1196bddc8d9704d742b03421d59839d9f3
ohanSuyal <144371692+Adirohansuyal@users.noreply.github.com>
2025-02-26 18:30:27 +0530

    new

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def train_model(model_type="Random Forest", n_estimators=100, kernel="rbf"):
     return model, accuracy
 
 # App Layout
-st.title("🌺 Iris Flower Classification Modell")
+st.title("🌺 Iris Flower Classification Model by adi")
 st.write("A Streamlit app demonstrating GitHub Actions with automated feature updates (CI/CD).")
 
 # Sidebar Navigation
